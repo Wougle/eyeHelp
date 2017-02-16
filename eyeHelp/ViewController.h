7e66cd6ee6cc6e253a891bf9215ab00e03c5a2a2
吴戈 <Wougle@wugedeMacBook-Pro.local>
2017-02-16 09:14:18 +0800

    Initial Commit

---
+++ eyeHelp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  eyeHelp
+//
+//  Created by 吴戈 on 2017/2/16.
+//  Copyright © 2017年 吴戈. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ eyeHelp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  eyeHelp
+//
+//  Created by 吴戈 on 2017/2/16.
+//  Copyright © 2017年 吴戈. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
